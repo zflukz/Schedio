eaffd0627a27278a7a7caadc359f54661498b023
zFlukz <tanaphat255900@gmail.com>
2025-10-14 16:47:50 +0700

    Hide_secret_value

---
+++ Server/src/main/java/com/example/demo/interfaces/IUserService.java
@@ -0,0 +1,22 @@
+package com.example.demo.interfaces;
+
+import com.example.demo.controller.dto.CreateUserDto;
+
+import com.example.demo.entity.Users;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IUserService {
+
+    Users createUser(CreateUserDto body);
+    List<Users> getAll();
+    Users getByID(UUID id);
+    Users updateUserByID(UUID id, CreateUserDto body);
+    void deleteUserByID(UUID id);
+
+
+
+
+
+}
