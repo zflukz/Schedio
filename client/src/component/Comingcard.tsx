ac25af602db1308755d92ce0ee036f94510dbeb0
SaMuii72 <113111313+SaMuii72@users.noreply.github.com>
2025-10-15 16:58:24 +0700

    Update Signin and Eventcard

--- client/src/component/Comingcard.tsx
+++ client/src/component/Comingcard.tsx
@@ -32,7 +32,7 @@ const Upcomingcard: React.FC<EventDetailProps> = ({
       </div>
       <div className="mb-[15px]">
         <h1 className="text-[20px] font-bold mb-[10px] text-primary">{title}</h1>
-        <div className="flex items-center text-black font-semibold mb-[10px]">
+        <div className="flex items-center text-text-black font-semibold mb-[10px]">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 mr-[10px]">
             <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clipRule="evenodd" />
           </svg>
@@ -42,7 +42,7 @@ const Upcomingcard: React.FC<EventDetailProps> = ({
           </svg>
           {time}
         </div>
-      <div className="flex items-center text-black font-semibold mb-[15px]">
+      <div className="flex items-center text-text-black font-semibold mb-[15px]">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-[10px]">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
           <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
--- client/src/page/Home.tsx
+++ client/src/page/Home.tsx
@@ -1,16 +1,43 @@
 import Navbar from '../component/Navbar';
-import Upcomingcard from '../component/Comingcard';
 import HorizontalScrollCards from '../component/HorizontalScrollCards';
+import Eventcard from '../component/Eventcard';
 
 interface User {
   name: string;
   role: 'admin' | 'organizer' | 'user';
 }
 
-
 function Home() {
   const user: User | null = null; // สมมติยังไม่ได้ login
-
+  const events = [
+      {
+        title: "Cooking Chicken",
+        duration: "2 hr.",
+        date: "20 Oct 2025",
+        time: "10:00 - 12:00 ",
+        location: "CB4301",
+        tags: ["art", "culture", "festival"],
+        imageUrl: "https://cdn.discordapp.com/attachments/1187005922288087091/1420375835528921108/IMG_0811_2.jpeg?ex=68f03254&is=68eee0d4&hm=f894f0516b7ae90d87cb75eff9b7d1a0dafb526dac65605c25a789a15839e8ba&",
+      },
+      {
+        title: "Chicken Language",
+        duration: "2 hr.",
+        date: "10 Nov 2025",
+        time: "13:00 - 15:00 ",
+        location: "CB1201",
+        tags: ["tech", "networking"],
+        imageUrl: "https://cdn.discordapp.com/attachments/1176389332140048465/1427957333304021033/7B70C761-1AB0-4A3D-BD48-7CBD581EDB4F_1_105_c.jpeg?ex=68f0c066&is=68ef6ee6&hm=b4defb4bb82934ecb05c67dba93e5efb7acb31693b16d9e4ed8698a08dc133b9&",
+      },
+      {
+        title: "Chicken Dance",
+        duration: "2 hr.",
+        date: "10 Nov 2025",
+        time: "13:00 - 15:00 ",
+        location: "CB1201",
+        tags: ["tech", "networking"],
+        imageUrl: "https://cdn.discordapp.com/attachments/1176389332140048465/1427957271392026634/1A10E491-6609-44BE-A69E-FE5B01391B98_1_105_c.jpeg?ex=68f0c057&is=68ef6ed7&hm=77460ee75f2a92ffa01c9ef5fc9f8f8cbc0552dfb66a928da71583fd0987e9b7&",
+      },
+    ];
 
   return (
     <div className="font-sans bg-background min-h-screen pt-[50px]">
@@ -23,7 +50,24 @@ function Home() {
       </div>
       <div className='flex items-center justify-center whitespace-nowrap pt-[85px] pb-[30px] text-[40px] font-bold mx-auto min-w-[500px]'>
 		      “ Find the Right Event for You ”
+      </div>
+      <div className="flex justify-center pb-[60px]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[30px] max-w-[1200px]">
+          {events.map((event, index) => (
+            <Eventcard
+              key={index}
+              title={event.title}
+              duration={event.duration}
+              date={event.date}
+              time={event.time}
+              location={event.location}
+              tags={event.tags}
+              imageUrl={event.imageUrl}
+              onViewMore={() => console.log(`Viewing ${event.title}`)}
+            />
+          ))}
         </div>
+      </div>
     </div>
   );
 }
