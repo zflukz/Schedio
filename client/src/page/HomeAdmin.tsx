37d22c635625b9a769521b69965fc48ac7a14d28
SaMuii72 <113111313+SaMuii72@users.noreply.github.com>
2025-11-15 16:38:18 +0700

    Fix HomeAdmin with correct ui

--- client/src/page/HomeAdmin.tsx
+++ client/src/page/HomeAdmin.tsx
@@ -95,16 +95,7 @@ console.log("User in HomeAdmin:", user);
 	  <div className="pt-[30px] px-4 sm:px-6">
         <div className="max-w-[1200px] mx-auto bg-white p-[25px] rounded-2xl shadow-sm py-[20px] ">
 		  <div className="flex flex-row items-start justify-between mb-6 gap-4">
-			<h2 className="text-[30px] font-semibold">My Events ({events.length})</h2>
-			<button
-			  className="flex gap-[10px] bg-night-default hover:bg-night-hover text-white font-semibold px-[15px] py-[8px] rounded-[8px] flex-shrink-0"
-			  onClick={() => navigate("/organizer/create event")}
-			>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-			<path fillRule="evenodd" d="M19.5 21a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-5.379a.75.75 0 0 1-.53-.22L11.47 3.66A2.25 2.25 0 0 0 9.879 3H4.5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h15Zm-6.75-10.5a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V10.5Z" clipRule="evenodd" />
-			</svg>
-			  Create Event
-			</button>
+			<h2 className="text-[30px] font-semibold">Events Management</h2>
 		  </div>
 
 		<EventFilterbar
